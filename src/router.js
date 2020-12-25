c8b8f8a22968ad01a249b1b2d5338e963c10160b
Lorena <criado.manzaneque@gmail.com>
2020-12-25 14:09:48 +0100

    create views, assets and components

---
+++ src/router.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home";
+import About from "@/views/About";
+import Error from "@/views/Error";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About
+    },
+    {
+      path: "*",
+      name: "Error",
+      component: Error
+    }
+  ]
+});
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Vue App Exchange</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener las cotizaciones de las cryptomonedas mas
+      importantes a traves de la API REST de Coincap.
+    </p>
+
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline"
+      >Volver a la pagina de Inicio</router-link
+    >
+  </div>
+</template>
