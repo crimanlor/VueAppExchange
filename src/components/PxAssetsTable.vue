7abae5a22bf044779567855c3a0433b85834b5c8
Lorena <criado.manzaneque@gmail.com>
2020-12-25 21:17:35 +0100

    refactoring with lint

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -21,6 +21,7 @@
       >
         <td>
           <img
+            class="w-6 h-6"
             :src="
               `https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`
             "
@@ -33,7 +34,15 @@
         <td>{{ a.name }}</td>
         <td>{{ a.priceUsd | dollar }}</td>
         <td>{{ a.marketCapUsd | dollar }}</td>
-        <td>{{ a.changePercent24Hr | percent }}</td>
+        <td
+          v-bind:class="
+            a.changePercent24Hr.includes('-')
+              ? 'text-red-600'
+              : 'text-green-600'
+          "
+        >
+          {{ a.changePercent24Hr | percent }}
+        </td>
         <td class="hidden sm:block"></td>
       </tr>
     </tbody>
