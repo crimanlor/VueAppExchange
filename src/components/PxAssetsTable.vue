52fd62aba28bd73fa739fd74c541dfde240d1ebd
Lorena <criado.manzaneque@gmail.com>
2020-12-25 18:24:38 +0100

    create api.js and filters.js

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -15,14 +15,25 @@
     </thead>
     <tbody>
       <tr
+        v-for="a in assets"
+        :key="a.id"
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
       >
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>
+          <img
+            :src="
+              `https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`
+            "
+            :alt="a.name"
+          />
+        </td>
+        <td>
+          <b>#{{ a.rank }}</b>
+        </td>
+        <td>{{ a.name }}</td>
+        <td>{{ a.priceUsd | dollar }}</td>
+        <td>{{ a.marketCapUsd | dollar }}</td>
+        <td>{{ a.changePercent24Hr | percent }}</td>
         <td class="hidden sm:block"></td>
       </tr>
     </tbody>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <px-assets-table />
+    <px-assets-table :assets="assets" />
   </div>
 </template>
 
 <script>
+import api from "@/api";
 import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
   name: "Home",
-  components: { PxAssetsTable }
+  components: { PxAssetsTable },
+
+  data() {
+    return {
+      assets: []
+    };
+  },
+
+  created() {
+    api.getAssets().then(assets => (this.assets = assets));
+  }
 };
 </script>
